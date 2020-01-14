835310c3d4756e579e3225eb80ad9b2c89670738
Mvermouth <498801877@qq.com>
2020-01-14 12:06:02 +0800

    请求接口配置

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,8 @@ var log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
 logger.debug("Some debug messages");
-const baseReqUrl = "http://47.106.70.21:8000"
+const fs = require('fs')
+const baseReqUrl = JSON.parse(fs.readFileSync(`${__dirname}/../configs/baseUrl.json`));
 /*注册逻辑:
 1.验证 邀请码是否有效 invitation_code
 2.注册账号,返回id registration
@@ -15,17 +16,15 @@ router.post('/reg', async function(req, res, next) {
   //验证邀请码
   if(req.body && req.body.ratio){
     logger.info('邀请码:"%s"',req.body.ratio);
-    var data = await axios.get(`${baseReqUrl}/api/tbk/invitation_code/?code=${req.body.ratio}&status=0&deleted=false`);
-    console.log(data.data);
+    var data = await axios.get(`${baseReqUrl.robot}/api/tbk/invitation_code/?code=${req.body.ratio}&status=0&deleted=false`);
     logger.info('邀请码请求验证结果:"%s"',data.status);
     if(data.status == 200 && data.data && data.data.count == 1){
       logger.info('邀请码结果:"%s"',JSON.stringify(data.data.results));
       try{
-        var regRes = await axios.post(`${baseReqUrl}/api/auth/registration/`,{
+        var regRes = await axios.post(`${baseReqUrl.robot}/api/auth/registration/`,{
           username:req.body.user
           ,password:req.body.password
         });
-        console.log(regRes);
         if(regRes && regRes.status > 199 && regRes.status < 300){
           logger.info('注册请求验证结果:"%s"',regRes.status);
           logger.info('注册结果:"%s"',JSON.stringify(regRes.data));
@@ -34,7 +33,7 @@ router.post('/reg', async function(req, res, next) {
             user_id,
             invitation_code:req.body.ratio
           };
-          var lastRes = await axios.post(`${baseReqUrl}/api/tbk/reseller/`,lastDataReq);
+          var lastRes = await axios.post(`${baseReqUrl.robot}/api/tbk/reseller/`,lastDataReq);
           if(lastRes && lastRes.status > 199 && lastRes.status < 300){
             logger.info('reseller请求验证结果:"%s"',lastRes.status);
             logger.info('reseller结果:"%s"',JSON.stringify(lastRes.data));
@@ -53,12 +52,10 @@ router.post('/reg', async function(req, res, next) {
       } catch(e){
         logger.error('注册请求验证结果:"%s"',e);
       }
-      console.log();
     } else {
       logger.error('注册请求验证结果:"%s"',data.status);
     }
   }
-  //res.send("999")
     res.send({
       code:-1
       ,msg:"注册失败"
@@ -67,15 +64,13 @@ router.post('/reg', async function(req, res, next) {
 
 //登录
 router.post("/login",async function(req, res, next){
-  console.log(req.body);
   if(req.body && req.body.user && req.body.password){
     var sendData = {
       username:req.body.user
       ,password:req.body.password
     };
-    console.log(sendData);
     try{
-      var ress = await axios.post(`${baseReqUrl}/api/auth/login/`,sendData);
+      var ress = await axios.post(`${baseReqUrl.robot}/api/auth/login/`,sendData);
       logger.info('登录请求验证结果:"%s"',ress.status);
       logger.info('登录结果:"%s"',JSON.stringify(ress.data));  
       if(ress && ress.status == 200 && ress.data && ress.data.key){
@@ -97,7 +92,6 @@ router.post("/login",async function(req, res, next){
 
 //绑定
 router.post("/binding",async function(req, res, next){
-  console.log(req.body.data);
   if(!req.body.token){
     res.send({code:-1});
     return;
@@ -115,89 +109,56 @@ router.post("/binding",async function(req, res, next){
       }     
       var params = {
          method: "post"
-        ,url:`${baseReqUrl}/api/tbk/binding/`
+        ,url:`${baseReqUrl.robot}/api/tbk/binding/`
         ,data:sendData
         ,headers:{"Authorization": `Token ${req.body.token}`}       
       } 
       var ps = JSON.stringify(params);
       evals = eval(`axios(${ps})`);
       urls.push(evals);
-
-
-      console.log("_______________________________________________________")
-      console.log(sendData);
-      // var ress = await axios.post(`${baseReqUrl}/api/tbk/binding/`,sendData,{
-      //   "headers":{"Authorization": "Token 103e731394e08daf185621326fb42f38a2e2abbf"}
-      // });
-      // try{
-        // var ress = await axios({
-        //           method: 'post'
-        //           ,url: `${baseReqUrl}/api/tbk/binding/`
-        //           ,data: sendData
-        //           ,headers:{"Authorization": `Token ${req.body.token}`}
-        //         });    
-      //   //         //console.log(ress);
-      //   //         console.log(ress.data);  
-      //   var ress = await axios.all(urls);  
-      // } catch(e){
-      //   console.log(e);
-      //   logger.error('错误:"%s"绑定请求结果:"%s"',JSON.stringify(e.response.data));
-      // }
     }
 
     try{ 
       var ress = await axios.all(urls);  
-      console.log(ress);
       res.send({
         code:0
         ,data:ress.data
       });
     } catch(e){
-      console.log(e);
       logger.error('错误:"%s"绑定请求结果:"%s"',JSON.stringify(e.response.data));
       res.send({code:-1,msg:JSON.stringify(e.response.data)});
     }
-
-
-    // console.log(sendData);
-    // // var ress = await axios.post(`${baseReqUrl}/api/tbk/binding/`,sendData);
-    // res.send("999");
   }
 })
 //绑定列表
 router.post("/getBinding",async function(req, res, next){
-    console.log(req.body);
     var ress = await axios({
       method: 'get'
-      ,url: `${baseReqUrl}/api/tbk/binding/`
+      ,url: `${baseReqUrl.robot}/api/tbk/binding/`
       ,data: req.body.pobj
       ,headers:{"Authorization": `Token ${req.body.token}`}
     });    
-    console.log(ress.data);
     res.send(ress.data);
 });
 //收益列表
 router.post("/getCommission_fee",async function(req, res, next){
-    console.log(req.body);
   try{
     //获取reseller列表,带token过去,后端帮过滤
     var resellers = await axios({
       method: 'get'
-      ,url: `${baseReqUrl}/api/tbk/reseller/`
+      ,url: `${baseReqUrl.robot}/api/tbk/reseller/`
       //,data: req.body.pobj
       ,headers:{"Authorization": `Token ${req.body.token}`}
     });  
     if(resellers && resellers.data && resellers.data.result.length == 1){
       var reseller = resellers.data.result[0];
-      console.log(resellers.data); 
       req.body.pobj.user_id = reseller.user_id;
       var ress = await axios({
         method: 'get'
-        ,url: `${baseReqUrl}/api/tbk/commission_fee/`
+        ,url: `${baseReqUrl.robot}/api/tbk/commission_fee/`
         ,data: req.body.pobj
         ,headers:{"Authorization": `Token ${req.body.token}`}
       });    
-      console.log(ress.data);
       res.send(ress.data);
     }
   } catch(e){
--- routes/im.js
+++ routes/im.js
@@ -5,36 +5,16 @@ var log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
 logger.debug("Some debug messages");
-
+const fs = require('fs')
+const baseReqUrl = JSON.parse(fs.readFileSync(`${__dirname}/../configs/baseUrl.json`));
 /* GET home page. */
 router.post('/search-srv/im_group_views/page', async function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  console.log(req.body);
-  //res.send("999")
-  console.log("123");
-//  axios.post('http://120.77.202.187:5765/search-srv/im_group_views/page',{"filters":{"AND":[{"OR":[]},{"group.memberCount":{"NE":0}},{"tenantId":{"EQ":"dc90ef20143f437e8144b827c19ec6ea"}}]},"pageNo":1,"pageSize":20,"sort":[{"ignoreCase":false,"property":"ctime","type":"DSC"}]}
-// )
-//   .then(function (re) {
-//       //console.log(re);
-//       console.log("ppp");
-      // if(re && re.status == 200){
-      // 	console.log(re.data);
-      // }
-//       res.send(re.data)
-//   })
-//   .catch(function (error) {
-
-//   });
-	//var data = await axios.post('http://120.77.202.187:5765/search-srv/im_group_views/page',req.body.params);
-  var data = await axios.post('http://120.77.202.187:6111/im/group/page',{
+  var data = await axios.post(`${baseReqUrl.im}/im/group/page`,{
     "filters":{"OR": [{"ownerPlatformUid": {"EQ": req.body.customID}},{"ownerCustomID": {"EQ": req.body.customID}}]}
   });
-  var owner = await axios.post('http://120.77.202.187:6111/im/contact/page',{
+  var owner = await axios.post(`${baseReqUrl.im}/im/contact/page`,{
     "filters":{"OR": [{"platformUid": {"EQ": req.body.customID}},{"customID": {"EQ": req.body.customID}}]}
   });
-  //req.body.customID
-	console.log(data.data);
-  console.log(owner.data);
 	if(data && data.status == 200 && owner && owner.status == 200){
       logger.info('群主信息:"%s"',JSON.stringify(owner.data));
       if(data.data && data.data.data.content && data.data.data.content.length > 0 && owner.data.data.content.length > 0){
